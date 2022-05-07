75325714b0749a3f271902bad3497a300b609d1a
LeRoiBof <maximedupuis193@gmail.com>
2022-05-07 22:55:41 +0200

    Sound adjusted

--- src/main/java/org/scarab/Main.java
+++ src/main/java/org/scarab/Main.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.File;
 import java.io.FileInputStream;
 
 public class Main extends Application {
@@ -25,11 +28,16 @@ public class Main extends Application {
     @Override
     public void start(Stage _primaryStage) throws Exception {
         ///Stage init
-        this.primaryStage = _primaryStage;
+        primaryStage = _primaryStage;
         primaryStage.setTitle("Scarab Is You");
         primaryStage.getIcons().add(new Image (new FileInputStream("src/main/resources/assets/menu/scarablogo.jpg")));
         primaryStage.setWidth(1280);
         primaryStage.setHeight(720);
+        Media media = new Media(new File("src/main/resources/music/menu.mp3").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.setVolume(0.1);
         primaryStage.initStyle(StageStyle.UNDECORATED);
         root.setPadding(new Insets(20));
 
